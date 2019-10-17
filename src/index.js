2d163e8349337202003cc879f504f1007f9bdc40
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-17 08:42:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const description = React.createElement('p', {}, 'Who is learning React')
 const list = React.createElement('ul', {className:'my-interests'},
       [
         React.createElement('li', {}, 'JavaScript'),
-        React.createElement('li', {}, 'Vanilla'),
+        React.createElement('li', {}, 'React'),
         React.createElement('li', {}, 'Banana')
       ]
     )
