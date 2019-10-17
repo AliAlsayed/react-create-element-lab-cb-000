e89d1d683f0ff49d6c35b4c082daa8e6d39b940f
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-17 08:39:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 /*
@@ -10,3 +10,5 @@ ReactDOM.render(
 
 export default meInReact
 */
+const title = const meInReact = React.createElement('h1', {}, 'An Awesome Person' )
+const meInReact = React.createElement('div', {className: 'me'}, )
